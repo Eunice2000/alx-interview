5592aa86be869de3c919103a0133e36204d526c5
Eunice js <90851478+Eunice2000@users.noreply.github.com>
2023-07-05 19:26:21 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Pascal Trangle
+Pascal Triangle
 """
 
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
+    Returns a list of lists of integers representing Pascal's triangle of n.
     """
     triangle = []
 
